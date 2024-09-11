36c1be0d584a82ec77f5c3c305bc2bbc7d677884
91leejs <lgchoco@naver.com>
2024-09-11 17:46:02 +0900

    portfolio

--- js/jquery.bxslider.js
+++ js/jquery.bxslider.js
@@ -22,7 +22,7 @@
     captions: false,
     ticker: false,
     tickerHover: false,
-    adaptiveHeight: false,
+    adaptiveHeight: true,
     adaptiveHeightSpeed: 500,
     video: false,
     useCSS: true,
@@ -79,7 +79,7 @@
     minSlides: 1,
     maxSlides: 1,
     moveSlides: 0,
-    slideWidth: 0,
+    slideWidth: 1400,
     shrinkItems: false,
 
     // CALLBACKS
