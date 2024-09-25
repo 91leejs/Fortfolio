20f04bb14d75615414665da16c431d9feadf82de
91leejs <lgchoco@naver.com>
2024-09-25 09:46:14 +0900

    portfolio

--- js/port.js
+++ js/port.js
@@ -30,6 +30,50 @@ $(function(){
         autoScrolling: true,
         scrollHorizontally: true, 
         controlArrows: false,
+
+        afterLoad: function(origin, destination, direction) {
+            if (destination.index === 1) { // 두 번째 섹션
+
+                    $('.p_left').addClass('l_ani'); // 애니메이션 클래스 다시 추가
+                    $('.p_right').addClass('r_ani'); // 애니메이션 클래스 다시 추가
+            }
+            if (destination.index === 2) { // 세 번째 섹션
+
+                    $('.cont_inner').addClass('cont_move'); // 애니메이션 클래스 다시 추가
+            }
+            if (destination.index === 3) { // 네 번째 섹션
+                
+                $('.pi2').addClass('parallax'); // 애니메이션 클래스 다시 추가
+            }
+            if (destination.index === 4) { // 다섯 번째 섹션
+                
+                    $('.pi1').addClass('parallax'); // 애니메이션 클래스 다시 추가
+            }
+            if (destination.index === 5) { // 여섯 번째 섹션
+                
+                    $('.pi11').addClass('parallax'); // 애니메이션 클래스 다시 추가
+            }
+            if (destination.index === 6) { // 일곱 번째 섹션
+                
+                    $('.pi6').addClass('parallax'); // 애니메이션 클래스 다시 추가
+            }
+            if (destination.index === 7) { // 여덟 번째 섹션
+                
+                    $('.pi7').addClass('parallax'); // 애니메이션 클래스 다시 추가
+            }
+            if (destination.index === 8) { // 아홉 번째 섹션
+                
+                    $('.pi9').addClass('parallax'); // 애니메이션 클래스 다시 추가
+            }
+            if (destination.index === 9) { // 열 번째 섹션
+                
+                    $('.pi8').addClass('parallax'); // 애니메이션 클래스 다시 추가
+            }
+            if (destination.index === 10) { // 열한 번째 섹션
+                
+                    $('.pi10').addClass('parallax'); // 애니메이션 클래스 다시 추가
+            }
+        }
     })
 
 
